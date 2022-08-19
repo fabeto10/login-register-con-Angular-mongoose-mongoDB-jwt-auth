07012fec857960ad536b191ea1b9cb544b65e5b7
Francisco Vidal <f0414475@gmail.com>
2022-08-19 14:58:58 -0400

    creando auth.model.js terminando tabla

--- auth/auth.dao.js
+++ auth/auth.dao.js
@@ -6,4 +6,10 @@ authSchema.statics = {
         const user = new this( data );
         user.save(cb);
     },
+    login: function (query, cb){
+        this.find(query, cb);
+    }
 }
+
+const authModel = mongoose.model('Users', authSchema);
+module.exports = authModel;
